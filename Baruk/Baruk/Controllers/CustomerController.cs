d7368229728fcb8abdbb28e018b49058a1df389e
robq97 <39276231+robq97@users.noreply.github.com>
2019-07-13 01:06:48 -0600

    Backup

---
+++ Baruk/Baruk/Controllers/CustomerController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Baruk.Controllers
+{
+    public class CustomerController : Controller
+    {
+        // GET: Customer
+        public ActionResult NewCustomer()
+        {
+            return View();
+        }
+    }
+}
---
+++ Baruk/Baruk/Controllers/InformationController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Baruk.Controllers
+{
+    public class InformationController : Controller
+    {
+        // GET: Information
+        public ActionResult InformationForm()
+        {
+            return View();
+        }
+
+        // GET: Information
+        public ActionResult ChooseUs()
+        {
+            return View();
+        }
+
+        // GET: Information
+        public ActionResult WhoAreWe()
+        {
+            return View();
+        }
+
+        // GET: Information
+        public ActionResult Calendar()
+        {
+            return View();
+        }
+    }
+}
